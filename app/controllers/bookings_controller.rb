de0db286a2fdc050d89c8dcc33cf46707362ba62
Erez Friemagor <erezfree29@gmail.com>
2018-06-07 15:39:17 +0300

    create a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,25 +4,29 @@ class BookingsController < ApplicationController
   end
 
   def show
+    @booking = Booking.find(params[:id])
+ raise
   end
 
   def new
-
     @trainer = Trainer.find(params[:trainer_id])
-    @booking = Booking.new()
+    @booking = Booking.new
   end
 
   def create
-
     @user = current_user
     @trainer = Trainer.find(params[:trainer_id])
     @booking = Booking.new(booking_params)
 
     @booking.total_price = params[:booking][:hours].to_i * @trainer.hourly_rate
-    @booking.user_id = @user
-    @booking.trainer_id =@trainer
-    @booking.save
+    @booking.user_id = @user.id
+    @booking.trainer_id = @trainer.id
 
+    if @booking.save
+      redirect_to trainer_booking_path(@trainer, @booking)
+    else
+      render :new
+  end
   end
 
   def edit
@@ -37,10 +41,12 @@ end
 private
   def booking_params
 
-params.require(:booking).permit(:hours)
+params.require(:booking).permit(:hours,:time_of_booking)
 
 end
 
 
 
 end
+
+
