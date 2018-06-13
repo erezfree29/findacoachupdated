f9a7a897380f84569123b2860649b8bed7402c00
Erez Friemagor <erezfree29@gmail.com>
2018-06-13 19:41:02 +0300

    show_trainer

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,52 +1,50 @@
 class BookingsController < ApplicationController
   def index
    @booking = Booking.all
+ end
+ def show
+
+  @booking = Booking.find(params[:id])
+  @trainer = Trainer.where(id: @booking.trainer_id).first
+  @expertise = Expertise.where(id: @trainer.expertise_id).first
+end
+
+def new
+  @trainer = Trainer.find(params[:trainer_id])
+  @booking = Booking.new
+end
+
+def create
+  @user = current_user
+  @trainer = Trainer.find(params[:trainer_id])
+  @booking = Booking.new(booking_params)
+
+  @booking.total_price = params[:booking][:hours].to_i * @trainer.hourly_rate
+  @booking.user_id = @user.id
+  @booking.trainer_id = @trainer.id
+
+  if @booking.save
+    redirect_to trainer_booking_path(@trainer, @booking)
+  else
+    render :new
   end
+end
 
-  def show
+def edit
+end
 
-    @booking = Booking.find(params[:id])
-    @trainer = Trainer.where(id: @booking.trainer_id).first
-    @expertise = Expertise.where(id: @trainer.expertise_id).first
-  end
+def update
+end
 
-  def new
-    @trainer = Trainer.find(params[:trainer_id])
-    @booking = Booking.new
-  end
-
-  def create
-    @user = current_user
-    @trainer = Trainer.find(params[:trainer_id])
-    @booking = Booking.new(booking_params)
-
-    @booking.total_price = params[:booking][:hours].to_i * @trainer.hourly_rate
-    @booking.user_id = @user.id
-    @booking.trainer_id = @trainer.id
-
-    if @booking.save
-      redirect_to trainer_booking_path(@trainer, @booking)
-    else
-      render :new
-  end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+def destroy
 end
 
 private
-  def booking_params
+def booking_params
 
-params.require(:booking).permit(:hours,:time_of_booking)
+  params.require(:booking).permit(:hours,:time_of_booking)
 
 end
-
 
 
 end
