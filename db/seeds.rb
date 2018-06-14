ba99a25c581937d3bcfd45d7677481a5e8932ad0
Erez Friemagor <erezfree29@gmail.com>
2018-06-15 00:25:51 +0300

    show trainer

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,19 @@
 
 p "Destroying data....."
 
-Booking.destroy_all
 Trainer.destroy_all
+
+
+
+Booking.destroy_all
+
+
 Expertise.destroy_all
 
+
 User.destroy_all
+
+
 
 
 
@@ -64,7 +72,7 @@ first_name: "Adi", last_name: "mommy", age: 21, gender: "female",
 address:"best place in tel aviv",photo:"image/upload/v1528889148/uclxkxou0gwwgurcbemo.jpg")
 
 
-trainer2 =Trainer.create(user_id: 2,expertise_id: 3, hourly_rate: 90,
+trainer2 =Trainer.create(user_id: 1,expertise_id: 3, hourly_rate: 90,
 
 
 first_name: "albert", last_name: "einstein", age: 120, gender: "female",
@@ -72,31 +80,31 @@ first_name: "albert", last_name: "einstein", age: 120, gender: "female",
 address: "32 ben yeuda street tel aviv",photo:"image/upload/v1528889503/vxfmstmh6nxz5viuazto.jpg")
 
 
-trainer3 =Trainer.create(user_id: 2,expertise_id: 4, hourly_rate: 12,
+trainer3 =Trainer.create(user_id: 1,expertise_id: 4, hourly_rate: 12,
 
 first_name: "Elton", last_name: "John", age: 120, gender: "male",
 
 address: "beach of La Concha San Sebastián",photo:"image/upload/v1528889741/eh4eftmayrwss4nkphus.jpg")
 
-trainer4 =Trainer.create(user_id: 4,expertise_id: 4, hourly_rate: 12,
+trainer4 =Trainer.create(user_id: 1,expertise_id: 4, hourly_rate: 12,
 
 first_name: "Aarav", last_name: "Lamba", age: 14, gender: "female",
 
 address: "india Mumbai 1020 buddha street",photo:"image/upload/v1528890155/i6xjauhylfez20tzbfha.jpg")
 
-trainer5 =Trainer.create(user_id: 6,expertise_id: 6, hourly_rate: 60,
+trainer5 =Trainer.create(user_id: 1,expertise_id: 6, hourly_rate: 60,
 
 first_name: "Michel", last_name: "Jackson", age: 14, gender: "female",
 
 address: "india Mumbai 1020 buddha street",photo:"image/upload/v1528890506/bd6ahgwwik6dlpsxnaxe.jpg",)
 
-trainer6 =Trainer.create(user_id: 5,expertise_id: 6, hourly_rate: 88,
+trainer6 =Trainer.create(user_id: 1,expertise_id: 6, hourly_rate: 88,
 
 first_name: "Birdigit", last_name: "welsh", age: 14, gender: "female",
 
 address: "Sunny England",photo:"image/upload/v1528890775/be5q2fsuxjka01uqygpe.jpg")
 
-trainer7=Trainer.create(user_id: 2,expertise_id: 6, hourly_rate: 25,
+trainer7=Trainer.create(user_id: 1,expertise_id: 6, hourly_rate: 25,
 
 first_name: "Sara", last_name: "The mighty", age: 14, gender: "female",
 
