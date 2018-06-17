15f4fc1edb161342345543e114203c69fbfb54a0
Erez Friemagor <erezfree29@gmail.com>
2018-06-17 14:05:31 +0300

    show trainer's

--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,5 +1,6 @@
 class Trainer < ApplicationRecord
   belongs_to :user
+  belongs_to :expertise
   has_many :booking
   has_many :reviews, through: :bookings
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 1 }
