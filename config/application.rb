43cdfc4910b1b1d37760c6c3d362fb8ff8f0d773
Erez Friemagor <erezfree29@gmail.com>
2018-06-28 19:43:47 +0300

    m

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module TrainerApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
 end
-config.assets.initialize_on_precompile = false
+
