42bceb047ead323dbec544d093accfad74ea5d01
Erez Friemagor <erezfree29@gmail.com>
2018-08-06 22:29:51 +0100

    m

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,6 +2,7 @@ class CalendarController < ApplicationController
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
 
+    @trainer = Trainer.find(params["format"])
 
 
   end
