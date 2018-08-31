ef095f8be8758d7974379a2c08647ca8af6e6202
erez friemagor <erezfree29@gmail.com>
2018-08-31 19:52:18 +0100

    dropdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,17 +19,27 @@
 p 'Creating expertises'
 
 
-expertise = Expertise.new(name: "weight loss")
+expertise = Expertise.new(name: "life Coaching")
 expertise.save!
-expertise = Expertise.new(name: "cardio")
+expertise = Expertise.new(name: "Health coaching")
 expertise.save!
-expertise = Expertise.new(name: "aerobics")
+expertise = Expertise.new(name: "Relationship coaching")
 expertise.save!
-expertise = Expertise.new(name: "physical recovery")
+expertise = Expertise.new(name: "Family coaching")
 expertise.save!
-expertise = Expertise.new(name: "swimming")
+expertise = Expertise.new(name: "Communication coaching")
 expertise.save!
-expertise = Expertise.new(name: "posture")
+expertise = Expertise.new(name: "Image coaching")
+expertise.save!
+expertise = Expertise.new(name: "Image coaching")
+expertise.save!
+expertise = Expertise.new(name: "Retirement coaching")
+expertise.save!
+expertise = Expertise.new(name: "Career coaching")
+expertise.save!
+expertise = Expertise.new(name: "Business coaching")
+expertise.save!
+expertise = Expertise.new(name: "Leadership coaching")
 expertise.save!
 
 
