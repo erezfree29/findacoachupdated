c24eaa4459bbebd56b2364ad619c2243bc0d9aed
erez friemagor <erezfree29@gmail.com>
2018-09-27 09:56:53 +0100

    skip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-
+protect_from_forgery
+before_action :authenticate_user!
   def default_url_options
   { host: ENV["HOST"] || "localhost:3000" }
  end
